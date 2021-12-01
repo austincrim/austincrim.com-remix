bbad458be748a1f51c5fb882c41e21955f2f9a79
Austin Crim <aust.crim@gmail.com>
2021-11-30 18:23:30 -0600

    loading=lazy

--- app/components/Appearance.tsx
+++ app/components/Appearance.tsx
@@ -8,6 +8,7 @@ export default function Appearance({ appearance }: { appearance: TAppearance }) 
           className="w-[800px] h-auto object-cover group-hover:scale-105 duration-500 transition-transform"
           src={appearance.imgUrl}
           alt={appearance.imgAlt}
+          loading="lazy"
         />
         <span className="absolute bottom-0 inline-block w-full p-4 text-xl font-semibold text-white transition-colors bg-gradient-to-t from-black via-black to-transparent group-hover:text-gray-200">
           {appearance.title}
