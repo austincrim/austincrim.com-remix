ead1ae67b57c131d12d9a40969590c7f8e6d3562
Austin Crim <aust.crim@gmail.com>
2021-12-14 19:52:10 -0600

    appearance pills

--- app/components/Appearance.tsx
+++ app/components/Appearance.tsx
@@ -6,13 +6,16 @@ export default function Appearance({
   appearance: TAppearance
 }) {
   return (
-    <div className="flex flex-col gap-2 appearance md:flex-row md:justify-between md:items-start">
+    <div className="flex flex-col gap-4 appearance md:flex-row md:justify-between md:items-start">
       <a
         className="text-xl leading-normal transition-colors text-secondary hover:text-primary"
         href={appearance.appearanceUrl}
       >
         {appearance.title}
       </a>
+      <span className="bg-off-base shadow w-[fit-content] px-4 rounded-full whitespace-nowrap">
+        {appearance.origin}
+      </span>
     </div>
   )
 }
